a4eba7b256c361ce13d1c689ecc959b8524d20db
root <root@qq.com>
2020-06-17 19:41:43 +0800

    把本地工程添加到git

---
+++ git-day01/src/com/itheima/pojo/GitDemo1.java
@@ -0,0 +1,12 @@
+package com.itheima.pojo;
+
+public class GitDemo1 {
+    public static void main(String[] args) {
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+        System.out.println("hello git!!!");
+    }
+}
